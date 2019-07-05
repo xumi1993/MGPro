7ef77a39e97dac7c619cdaf0be658705bcb28e81
Mijian Xu <mijian.xu@smail.nju.edu.cn>
2019-07-05 04:36:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ packages = find_packages()
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.1.8"
+VERSION = "0.1.9"
 setup(name='mgpro',
       version=VERSION,
       author='Mijian Xu',
